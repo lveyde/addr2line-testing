d7627963f6e68e44abfb32ca1823a38540d7632f
Lev Veyde <lveyde@gmail.com>
2023-02-19 01:33:23 +0200

    Initial addr2line test version
    Signed-off-by: Lev Veyde <lveyde@gmail.com>

---
+++ addr2line.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	addr2line "github.com/elazarl/addr2line"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+	"sync"
+)
+
+// Sql connection configuration
+type Connect_token struct {
+	DBDriver string
+	DBDSN    string
+}
+
+// Connects the target db and returns the handle
+func Connect_db(t *Connect_token) *sql.DB {
+	fmt.Println("connect")
+	db, err := sql.Open((*t).DBDriver, (*t).DBDSN)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected")
+	return db
+}
+
+type Symbols struct {
+	symbolName    string
+	symbolAddress string
+}
+
+func getSymbols(context *Context, instanceID string) ([]Symbols, error) {
+	query := fmt.Sprintf("SELECT symbol_name, symbol_address FROM symbols WHERE symbol_instance_id_ref = %s", instanceID)
+	fmt.Println("Running query", query)
+	rows, err := (*context).DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// A symbols slice to hold data from returned rows.
+	var symbols []Symbols
+
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var symbol Symbols
+		if err := rows.Scan(&symbol.symbolName, &symbol.symbolAddress); err != nil {
+			return symbols, err
+		}
+		symbols = append(symbols, symbol)
+	}
+	if err = rows.Err(); err != nil {
+		return symbols, err
+	}
+	return symbols, nil
+}
+
+// Context type
+type Context struct {
+	a2l         *addr2line.Addr2line
+	ch_workload chan Workload
+	mu          sync.Mutex
+	DB          *sql.DB
+}
+
+type Workload struct {
+	Addr2ln_offset string
+	Addr2ln_name   string
+}
+
+func A2L_resolver__init(fn string, DB_inst *sql.DB, includeInline bool) *Context {
+	a, err := addr2line.New(fn)
+	if err != nil {
+		panic(err)
+	}
+	addresses := make(chan Workload, 16)
+	context := &Context{a2l: a, ch_workload: addresses, DB: DB_inst}
+
+	go workload(context, includeInline)
+
+	return context
+}
+
+func workload(context *Context, includeInline bool) {
+	var e Workload
+
+	for {
+		e = <-context.ch_workload
+		context.mu.Lock()
+		rs, err := context.a2l.ResolveString(e.Addr2ln_offset)
+		context.mu.Unlock()
+
+		if err == nil {
+			fmt.Println(e.Addr2ln_offset, ":", rs[0].Function, "@", rs[0].File, rs[0].Line)
+
+			if includeInline {
+				for _, a := range rs[1:] {
+					fmt.Println(e.Addr2ln_offset, ":", "Inlined by", a.Function, "@", a.File, a.Line)
+				}
+			}
+		} else {
+			fmt.Println("Error resolving address", e.Addr2ln_offset)
+		}
+	}
+}
+
+func pushIntoQueue(ctx *Context, Q_WL *Workload) {
+	(*ctx).ch_workload <- *Q_WL
+}
+
+func main() {
+	var wl *Workload
+
+	var dbDriver, dbDSN, instanceID string
+	includeInline := false
+
+	dbDriver = os.Args[1]
+	dbDSN = os.Args[2]
+	instanceID = os.Args[3]
+
+	if os.Args[4] == "true" {
+		includeInline = true
+	}
+
+	t := Connect_token{dbDriver, dbDSN}
+	context := A2L_resolver__init("vmlinux", Connect_db(&t), includeInline)
+
+	symbols, err := getSymbols(context, instanceID)
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	fmt.Println("Size of symbols:", len(symbols))
+
+	file, err := os.Create("addr2line_Symbols.txt")
+	defer file.Close()
+
+	for _, symbol := range symbols {
+		wl = &Workload{Addr2ln_name: symbol.symbolName, Addr2ln_offset: symbol.symbolAddress}
+		file.Write([]byte(fmt.Sprintf("%s : %s\n", symbol.symbolAddress, symbol.symbolName)))
+		pushIntoQueue(context, wl)
+	}
+	file.Sync()
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module main
+
+go 1.18
+
+require (
+	github.com/elazarl/addr2line v0.0.0-20221220140718-9f1e6a7882b4 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+)
